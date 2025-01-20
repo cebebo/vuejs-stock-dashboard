bc8e100745225aa4232498c2fd6adf19de104711
cebebo <156794007+cebebo@users.noreply.github.com>
2025-01-20 19:02:00 +0100

    created first component

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,18 @@
 <template>
-  <div id="app">
-
-  </div>
+  <BaseCard>
+    <p>Hallo Welt</p>
+  </BaseCard>
 </template>
 
 <script>
 
+
+import BaseCard from "./components/BaseCard.vue";
+
 export default {
   name: 'App',
   components: {
-    
+    BaseCard   
   }
 }
 </script>
@@ -23,5 +26,7 @@ body {
   background: radial-gradient(71.11% 100% at 50% 0%, #020204 14.6%, #011F35 100%);
   width: 100vw;
   min-height: 100vh;
+  padding: 100px;
+  box-sizing: border-box;
 }
 </style>
---
+++ src/components/BaseCard.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="base-card">
+      <slot></slot>  <!-- Hier kann benutzerdefinierter HTML-Code eingefügt werden -->
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    name: 'BaseCard',
+  }
+  </script>
+  
+  <style scoped>
+  .base-card {
+    background-color: #011F35;
+    border-radius: 16px;
+    padding: 24px 32px;
+  }
+  </style>
+  
